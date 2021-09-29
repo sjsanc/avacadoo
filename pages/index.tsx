d8d75ac8154978be6143cd06b1eabb8326406adc
sjsanc <52794806+sjsanc@users.noreply.github.com>
2021-09-29 16:30:20 +0100

    saved wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,45 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import avoLogo from "../public/avacadoo.png";
 
-
 import create from "zustand"
 
-// const useStore = create(set => ({
-//   counter: 0,
-//   increase: () => set(state => ({ counter: state.counter + 1})),
-//   clear: () => set({ counter: 0 }),
-// }))
+interface StoreState {
+  data: any, 
+  updateStore: any
+}
 
-// const Counter = () => {
-//   const count = useStore(state => state.counter);
-//   return <h1>{count}</h1>
-// }
-
-// const Increaser = () => {
-//   const increase = useStore(state => state.increase);
-//   return <button onClick={increase}>UP!</button>
-// }
+const useStore = create<StoreState>(set => ({
+  data: {},
+  updateStore: (newState: any) => set(state => ({ data: {...state.data, ...newState }}))
+}))
 
 const Home: NextPage = () => {
+  const data = useStore(state => state.data);
+  const updateStore = useStore(state => state.updateStore);
+
   return (
     <div className="container">
+      <button onClick={() => {
+        updateStore({ [Math.random() * 10]: Math.random() * 10 })
+      }}>CLICK</button>
+      <div>{ JSON.stringify(data)}</div>
       <div className="flex items-center justify-center flex-col">
+        <div>
+          <div>Avocado Icon/Home</div>
+          <div>User Account Icon</div>
+          <div>Theme Picker</div>
+        </div>
         <div className="w-44">
           <Image src={avoLogo} />        
         </div>
         <h1 className="text-4xl">Avacadoo Study Guide</h1>
+        <label>Select Topic</label>
+        <select>
+          <option>AAT - Level 3</option>
+        </select>
+        <input type="submit" value="View topic summary" />
+        <label>Search Topic</label>
+        <input type="text" placeholder="E.g. Economic Order Quantity..." />
       </div>
     </div>
   )
